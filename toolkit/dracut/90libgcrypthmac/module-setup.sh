c37862cc0481901b9455ccfaa722fb70e7481577
李闻 <michael@liwendeMacBook-Pro.local>
2016-08-12 19:02:37 +0800

    xx

---
+++ toolkit/dracut/90libgcrypthmac/module-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+check() {
+    # a live host-only image doesn't really make a lot of sense
+    [[ $hostonly ]] && return 1
+    return 255
+}
+
+depends() {
+    # if dmsetup is not installed, then we cannot support fedora/red hat
+    # style live images
+    return 0
+}
+
+installkernel() {
+    return 0
+}
+
+install() {
+    inst_multiple /lib64/libgcrypt.so.20
+}
